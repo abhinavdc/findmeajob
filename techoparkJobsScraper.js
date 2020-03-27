c143613552eacfa79dfa3f6b0e8b93b14c4a336b
abhinavdc <abhinavdinesh95@gmail.com>
2020-03-28 01:59:31 +0530

    refactor

--- techoparkJobsScraper.js
+++ techoparkJobsScraper.js
@@ -4,6 +4,82 @@ const baseUrl = 'https://www.technopark.org/';
 const allJobEnteries = [];
 const numberOfEnteriesToFetch = 5;
 
+rp({ url: baseUrl + 'job-search', "rejectUnauthorized": false })
+    .then(async (html) => {
+        const jobList = parseJobListPage(html);
+
+        await fetchAndParseJobDetailPage(jobList);
+
+        for (i = 0; i < numberOfEnteriesToFetch; i++) {
+            const entry = {
+                jobTitle: jobList.jobTitles[i],
+                companyName: jobList.companyNames[i],
+                closingDate: jobList.closingDates[i],
+                jobUrl: jobList.jobUrls[i],
+                companyUrl: jobList.companyUrls[i],
+                jobDescription: jobList.jobDescriptions[i]
+            }
+            allJobEnteries.push(entry);
+        }
+        console.log(allJobEnteries);
+    })
+    .catch(function (err) {
+        //handle error
+        console.log(err)
+    });
+
+
+async function fetchAndParseJobDetailPage(jobList) {
+    await asyncForEach(jobList.jobUrls.slice(0, numberOfEnteriesToFetch), async (url, i) => {
+        // Fetch Job details
+        const jobDetailPageHtml = await rp({ url: baseUrl + url, "rejectUnauthorized": false });
+
+        const jobDescription = parseJobDetailPage(jobDetailPageHtml);
+        jobList.jobDescriptions.push(jobDescription);
+        // Show in console the progress of parsing
+        console.log(`Scraping ${i + 1}/${numberOfEnteriesToFetch}`);
+    });
+}
+
+function parseJobListPage(html) {
+    const jobList = {
+        jobTitles: [],
+        companyNames: [],
+        closingDates: [],
+        jobUrls: [],
+        companyUrls: [],
+        jobDescriptions: []
+    };
+
+    // Parse Job Title, Job Url
+    $('.jobTitleLink', html).each((i, elem) => {
+        jobList.jobTitles.push($(elem).text());
+        jobList.jobUrls.push($(elem).attr('href'));
+    });
+    // Parse company name, company url
+    $('.companyList > td:nth-child(2) > a', html).each((i, elem) => {
+        jobList.companyNames.push($(elem).text());
+        // remove '/' at the begining of the url
+        jobList.companyUrls.push($(elem).attr('href').substring(1));
+    });
+    // Parse closing date
+    $('.companyList > td:nth-child(3)', html).each((i, elem) => {
+        jobList.closingDates.push(convertDate($(elem).text()));
+    });
+    
+    return jobList;
+}
+
+function parseJobDetailPage(jobDetailPageHtml) {
+    const jobDescription = {
+        postingDate: $('.arrived.det-text.group-effect1 > div:nth-child(3) > p:nth-child(2)', jobDetailPageHtml).text(),
+        contactEmail: $('.arrived.det-text.group-effect1 > div:nth-child(5) > a', jobDetailPageHtml).text(),
+        briefDescription: $('.arrived.det-text.group-effect1 > div:nth-child(7) > p:nth-child(2)', jobDetailPageHtml).text(),
+        preferredSkills: $('.arrived.det-text.group-effect1 > div:nth-child(9) > p:nth-child(3)', jobDetailPageHtml).text()
+    };
+    return jobDescription;
+}
+
 function convertDate(dateString) {
     var dateParts = dateString.split("/");
 
@@ -18,78 +94,4 @@ async function asyncForEach(array, callback) {
         await callback(array[index], index, array);
     }
 }
-
-
-rp({ url: baseUrl + 'job-search', "rejectUnauthorized": false })
-    .then(async (html) => {
-        const jobTitles = [];
-        const companyNames = [];
-        const closingDates = [];
-        const jobUrls = [];
-        const companyUrls = [];
-        const jobDescriptions = [];
-
-        // Parse Job Title, Job Url
-        $('.jobTitleLink', html).each((i, elem) => {
-            jobTitles.push($(elem).text());
-            jobUrls.push($(elem).attr('href'));
-        });
-
-        // Parse company name, company url
-        $('.companyList > td:nth-child(2) > a', html).each((i, elem) => {
-            companyNames.push($(elem).text());
-            // remove '/' at the begining of the url
-            companyUrls.push($(elem).attr('href').substring(1));
-        });
-
-        // Parse closing date
-        $('.companyList > td:nth-child(3)', html).each((i, elem) => {
-            closingDates.push(convertDate($(elem).text()));
-        });
-
-        await asyncForEach(jobUrls.slice(0, numberOfEnteriesToFetch), async (url, i) => {
-            const jobDescription = {
-                postingDate: null,
-                contactEmail: '',
-                briefDescription: '',
-                preferredSkills: ''
-            };
-            const jobDetailPageHtml = await rp({ url: baseUrl + url, "rejectUnauthorized": false });
-            jobDescription.postingDate =
-                $('.arrived.det-text.group-effect1 > div:nth-child(3) > p:nth-child(2)', jobDetailPageHtml).text();
-            jobDescription.contactEmail =
-                $('.arrived.det-text.group-effect1 > div:nth-child(5) > a', jobDetailPageHtml).text();
-            
-            jobDescription.briefDescription =
-                $('.arrived.det-text.group-effect1 > div:nth-child(7) > p:nth-child(2)', jobDetailPageHtml).text();
-            
-            jobDescription.preferredSkills =
-                $('.arrived.det-text.group-effect1 > div:nth-child(9) > p:nth-child(3)', jobDetailPageHtml).text();
-
-            jobDescriptions.push(jobDescription);
-
-            console.log(`Scraping ${i+1}/${numberOfEnteriesToFetch}`);  
-        })
-
-        for (i = 0; i < numberOfEnteriesToFetch; i++) {
-            const entry = {
-                jobTitle: jobTitles[i],
-                companyName: companyNames[i],
-                closingDate: closingDates[i],
-                jobUrl: jobUrls[i],
-                companyUrl: companyUrls[i],
-                jobDescription: jobDescriptions[i]
-            }
-            allJobEnteries.push(entry);
-        }
-
-        console.log(allJobEnteries);
-
-
-    })
-    .catch(function (err) {
-        //handle error
-        console.log(err)
-    });
-
 
