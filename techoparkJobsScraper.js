64b63bb8ee0e0c213d54224648f2dcec30dfa0fa
abhinavdc <abhinavdinesh95@gmail.com>
2020-03-28 03:54:17 +0530

    formatting job description fields

--- techoparkJobsScraper.js
+++ techoparkJobsScraper.js
@@ -12,6 +12,7 @@ rp({ url: baseUrl + 'job-search', "rejectUnauthorized": false })
 
         for (i = 0; i < numberOfEnteriesToFetch; i++) {
             const entry = {
+                jobId: jobList.jobIds[i],
                 jobTitle: jobList.jobTitles[i],
                 companyName: jobList.companyNames[i],
                 closingDate: jobList.closingDates[i],
@@ -34,7 +35,14 @@ async function fetchAndParseJobDetailPage(jobList) {
         // Fetch Job details
         const jobDetailPageHtml = await rp({ url: baseUrl + url, "rejectUnauthorized": false });
 
-        const jobDescription = parseJobDetailPage(jobDetailPageHtml);
+        let jobDescription = {};
+
+        // Walking pages have different html structures, hence two different parsing logic is used
+        if (!jobList.jobTitles[i].includes('Walk in')) {
+            jobDescription = parseJobDetailPage(jobDetailPageHtml);
+        } else {
+            jobDescription = parseWalkinJobDetailPage(jobDetailPageHtml);
+        }
         jobList.jobDescriptions.push(jobDescription);
         // Show in console the progress of parsing
         console.log(`Scraping ${i + 1}/${numberOfEnteriesToFetch}`);
@@ -43,6 +51,7 @@ async function fetchAndParseJobDetailPage(jobList) {
 
 function parseJobListPage(html) {
     const jobList = {
+        jobIds: [],
         jobTitles: [],
         companyNames: [],
         closingDates: [],
@@ -55,6 +64,8 @@ function parseJobListPage(html) {
     $('.jobTitleLink', html).each((i, elem) => {
         jobList.jobTitles.push($(elem).text());
         jobList.jobUrls.push($(elem).attr('href'));
+        // Get vacancy_id from url
+        jobList.jobIds.push(+$(elem).attr('href').split('vacancy_id=')[1])
     });
     // Parse company name, company url
     $('.companyList > td:nth-child(2) > a', html).each((i, elem) => {
@@ -72,10 +83,56 @@ function parseJobListPage(html) {
 
 function parseJobDetailPage(jobDetailPageHtml) {
     const jobDescription = {
+        htmlRaw: $('.col-sm-8', jobDetailPageHtml).html(),
         postingDate: $('.arrived.det-text.group-effect1 > div:nth-child(3) > p:nth-child(2)', jobDetailPageHtml).text(),
         contactEmail: $('.arrived.det-text.group-effect1 > div:nth-child(5) > a', jobDetailPageHtml).text(),
-        briefDescription: $('.arrived.det-text.group-effect1 > div:nth-child(7) > p:nth-child(2)', jobDetailPageHtml).text(),
-        preferredSkills: $('.arrived.det-text.group-effect1 > div:nth-child(9) > p:nth-child(3)', jobDetailPageHtml).text()
+        briefDescriptionRaw: $('.arrived.det-text.group-effect1 > div:nth-child(7) > :not(.head)', jobDetailPageHtml).html(),
+        briefDescription: $('.arrived.det-text.group-effect1 > div:nth-child(7) > :not(.head)', jobDetailPageHtml)
+            .filter(':not(:contains(\'Job Description:\'))')
+            .text()
+            .replace(/\n\t/g, '')
+            .split('•')
+            .map((x) => x.trim())
+            .filter((x) => x)
+            .join(),
+        preferredSkillsRaw: $('.arrived.det-text.group-effect1 > div:nth-child(9) > :not(.head)', jobDetailPageHtml).text(),
+        preferredSkills: $('.arrived.det-text.group-effect1 > div:nth-child(9) > :not(.head)', jobDetailPageHtml)
+            .filter(':not(:contains(\'Experience:\')):not(:contains(\'Qualification:\'))')
+            .text()
+            .replace(/\n\t/g, '')
+            .split('•')
+            .map((x) => x.trim())
+            .filter((x) => x)
+            .join()
+    };
+    return jobDescription;
+}
+
+function parseWalkinJobDetailPage(jobDetailPageHtml) {
+    const jobDescription = {
+        postingDate: $('.arrived.det-text.group-effect1 > div:nth-child(3) > p:nth-child(2)', jobDetailPageHtml).text(),
+        walkinStartDate: $('.arrived.det-text.group-effect1 > div:nth-child(4) > p:nth-child(2)', jobDetailPageHtml).text(),
+        walkinClosingDate: $('.arrived.det-text.group-effect1 > div:nth-child(5) > p:nth-child(2)', jobDetailPageHtml).text(),
+        walkinTime: $('.arrived.det-text.group-effect1 > div:nth-child(6) > p:nth-child(2)', jobDetailPageHtml).text(),
+        contactEmail: $('.arrived.det-text.group-effect1 > div:nth-child(7) > a', jobDetailPageHtml).text(),
+        briefDescriptionRaw: $('.arrived.det-text.group-effect1 > div:nth-child(9) > :not(.head)', jobDetailPageHtml).html(),
+        briefDescription: $('.arrived.det-text.group-effect1 > div:nth-child(9) > :not(.head)', jobDetailPageHtml)
+            .filter(':not(:contains(\'Job Description:\'))')
+            .text()
+            .replace(/\n\t/g, '')
+            .split('•')
+            .map((x) => x.trim())
+            .filter((x) => x)
+            .join(),
+        preferredSkillsRaw: $('.arrived.det-text.group-effect1 > div:nth-child(11) > :not(.head)', jobDetailPageHtml).html(),
+        preferredSkills: $('.arrived.det-text.group-effect1 > div:nth-child(11) > :not(.head)', jobDetailPageHtml)
+            .filter(':not(:contains(\'Experience:\')):not(:contains(\'Qualification:\'))')
+            .text()
+            .replace(/\n\t/g, '')
+            .split('•')
+            .map((x) => x.trim())
+            .filter((x) => x)
+            .join()
     };
     return jobDescription;
 }
