6e0d16e0650740d2ee384e20dd652217ff4c1858
abhinavdc <abhinavdinesh95@gmail.com>
2020-03-27 18:26:49 +0530

    get tp jobs list

--- techoparkJobsScraper.js
+++ techoparkJobsScraper.js
@@ -2,26 +2,50 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'https://www.technopark.org/job-search';
 
+function convertDate(dateString) {
+    var dateParts = dateString.split("/");
+    
+    // month is 0-based, that's why we need dataParts[1] - 1
+    var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+
+    return dateObject
+}
+
 rp({ url, "rejectUnauthorized": false })
     .then(function (html) {
-        const jobNames = [];
+        const jobTitles = [];
         const companyNames = [];
-        const postingDates = [];
+        const closingDate = [];
         const jobLinks = [];
-        
-        //success!
-        jobNames.push($('.jobTitleLink', html).text());
-        // jobLinks.push($('.jobTitleLink', html).attr());
-        // const jobEntries = $('.jobTitleLink', html).attr('href');
+        const allJobEnteries = [];
 
-        // for (let i = 0; i < 45; i++) {
-        //     wikiUrls.push($('big > a', html)[i].attribs.href);
-        // }
-        // console.log(wikiUrls);
+        $('.jobTitleLink', html).each((i, elem) => {
+            jobTitles.push($(elem).text());
+        });
 
-        console.log(jobNames);
-        // console.log(jobLinks);
-        // console.log(Object.values(jobEntries));
+        $('.jobTitleLink', html).each((i, elem) => {
+            jobLinks.push($(elem).attr('href'));
+        });
+
+        $('.companyList > td:nth-child(2) > a', html).each((i, elem) => {
+            companyNames.push($(elem).text());
+        });
+
+        $('.companyList > td:nth-child(3)', html).each((i, elem) => {
+            closingDate.push(convertDate($(elem).text()));
+        });
+
+        for (i = 0; i < closingDate.length; i++) {
+            const entry = {
+                jobTitle: jobTitles[i],
+                companyName: companyNames[i],
+                closingDate: closingDate[i],
+                jobLink: jobLinks[i]  
+            }
+            allJobEnteries.push(entry);
+        }
+
+        console.log(allJobEnteries);
     })
     .catch(function (err) {
         //handle error
