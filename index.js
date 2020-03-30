cfa0a44c7d8cda7471c06dbb500ef895f06534d7
abhinavdc <abhinavdinesh95@gmail.com>
2020-03-30 14:53:07 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import db from './lib/db';
 import './lib/cron';
 
 const app = express();
+
+app.get(`/all-jobs`, async (req, res, next) => {
+  // get the scrape data
+  const { tpJobs } = db.value();
+
+  // respond with json
+  res.json(tpJobs);
+});
 
 app.listen(2093, () => {
   console.log(`Example App running on port http://localhost:2093`);
