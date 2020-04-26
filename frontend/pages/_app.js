05d9f1482644a64ebb468bc47101260f02356445
abhinavdc <abhinavdinesh95@gmail.com>
2020-04-27 03:28:14 +0530

    added bulma, mvp design

---
+++ frontend/pages/_app.js
@@ -0,0 +1,8 @@
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import '../style.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
