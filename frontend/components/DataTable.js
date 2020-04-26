66b69b4adcd92a152d6489d0de543c14227b9c39
abhinavdc <abhinavdinesh95@gmail.com>
2020-04-27 04:07:17 +0530

    display jobs in cards

--- frontend/components/DataTable.js
+++ frontend/components/DataTable.js
@@ -12,11 +12,11 @@ import { formatDistance } from 'date-fns';
 
 const useStyles = makeStyles({
   row: {
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
   container: {
-    maxHeight: '80vh'
-  }
+    maxHeight: '80vh',
+  },
 });
 
 export default function DataTable() {
@@ -27,44 +27,71 @@ export default function DataTable() {
     window.open('https://technopark.org/' + jobUrl, '_blank');
   };
   return (
-    <TableContainer component={Paper} className={classes.container}>
-      <Table stickyHeader aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>JobID</TableCell>
-            <TableCell>Job Title</TableCell>
-            <TableCell>Company</TableCell>
-            <TableCell>Posted</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {scrapeData.scrapes.map(row => (
-            <TableRow
-              hover
-              key={row.jobId}
-              onClick={() => rowClick(row)}
-              className={classes.row}
-            >
-              <TableCell component="th" scope="row">
-                {row.jobId}
-              </TableCell>
-              <TableCell>{row.jobTitle}</TableCell>
-              <TableCell>{row.companyName}</TableCell>
-              <TableCell>
-                {formatDistance(
-                  new Date(
-                    `${row.jobDescription.postingDate.split('/')[1]}/
+    <section class="section">
+      <div class="columns is-multiline">
+        {scrapeData.scrapes.map((row) => (
+          <div class="column is-2">
+            <div class="card is-clickable" onClick={() => rowClick(row)}>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content">
+                    <p class="subtitle is-6">{row.companyName}</p>
+                  </div>
+                </div>
+                <p class="subtitle">{row.jobTitle}</p>
+              </div>
+              <footer class="card-footer">
+                <div class="card-footer-item columns">
+                  <div class="column">
+                    <span class="is-size-7 has-text-grey-light has-text-left">
+                      {row.location}
+                    </span>
+                  </div>
+                  <div class="column">
+                    <span class="is-size-7 is-pulled-right has-text-info">
+                      {formatDistance(
+                        new Date(
+                          `${row.jobDescription.postingDate.split('/')[1]}/
                           ${row.jobDescription.postingDate.split('/')[0]}/
                           ${row.jobDescription.postingDate.split('/')[2]}`
-                  ),
-                  new Date()
-                )}{' '}
-                ago
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                        ),
+                        new Date()
+                      )}{' '}
+                      ago
+                    </span>
+                  </div>
+                </div>
+              </footer>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+
+    // {scrapeData.scrapes.map(row => (
+    //   <TableRow
+    //     hover
+    //     key={row.jobId}
+    //     onClick={() => rowClick(row)}
+    //     className={classes.row}
+    //   >
+    //     <TableCell component="th" scope="row">
+    //       {row.jobId}
+    //     </TableCell>
+    //     <TableCell>{row.jobTitle}</TableCell>
+    //     <TableCell>{row.companyName}</TableCell>
+    //     <TableCell>
+    //       {formatDistance(
+    //         new Date(
+    //           `${row.jobDescription.postingDate.split('/')[1]}/
+    //                 ${row.jobDescription.postingDate.split('/')[0]}/
+    //                 ${row.jobDescription.postingDate.split('/')[2]}`
+    //         ),
+    //         new Date()
+    //       )}{' '}
+    //       ago
+    //     </TableCell>
+    //   </TableRow>
+    // ))}
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -29,108 +29,34 @@ const useStyles = makeStyles({
 export default function Index() {
   const classes = useStyles();
   return (
-    <section>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </a>
-
-          <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-      </nav>
-      <section class="hero is-info is-bold">
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title is-size-2">Geek Jobs</h1>
-            <h2 class="subtitle is-size-6">
-              Hotest and latest jobs from Kerala's leading tech-parks
-            </h2>
-            <div class="columns is-centered">
-              <div class="column is-4">
-                <div class="control has-icons-right">
-                  <input
-                    class="input is-medium has-margin-right-7"
-                    type="text"
-                    placeholder="Find latest jobs"
-                  />
-                  <span class="icon is-medium is-right">
-                    <i class="fa fa-search"></i>
-                  </span>
+    <Page>
+      <section>
+        <section class="hero is-info is-bold">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 class="title is-size-2">Geek Jobs</h1>
+              <h2 class="subtitle is-size-6">
+                Hotest and latest jobs from Kerala's leading tech-parks
+              </h2>
+              <div class="columns is-centered">
+                <div class="column is-4">
+                  <div class="control has-icons-right">
+                    <input
+                      class="input is-medium has-margin-right-7"
+                      type="text"
+                      placeholder="Find latest jobs"
+                    />
+                    <span class="icon is-medium is-right">
+                      <i class="fa fa-search"></i>
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </section>
-      <section class="section">
-        <div class="columns is-multiline">
-          <div class="column is-2">
-            <div class="card">
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-6">Carestack</p>
-                  </div>
-                </div>
-                <p class="subtitle">Junior Software Engineer</p>
-              </div>
-              <footer class="card-footer">
-                <div class="card-footer-item columns">
-                  <div class="column">
-                    <span class="is-size-7 has-text-grey-light has-text-left">
-                      Trivandrum
-                    </span>
-                  </div>
-                  <div class="column">
-                    <span class="is-size-7 is-pulled-right has-text-info">
-                      2 days ago
-                    </span>
-                  </div>
-                </div>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </section>
-    </section>
-
-    // <Page>
-    //   <Head>
-    //     <title>Hello</title>
-    //     <link
-    //       rel="stylesheet"
-    //       href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    //     />
-    //     <link
-    //       rel="stylesheet"
-    //       href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    //     />
-    //   </Head>
-    //   <CssBaseline></CssBaseline>
-    //   <div className={classes.main}>
-    //     {/* <div className={classes.leftContainer}>
-    //       <Subscribe />
-    //     </div> */}
-    //     <div className={classes.rightContainer}>
-    //       <Search />
-    //       <DataTable />
-    //     </div>
-    //   </div>
-    // </Page>
+      <DataTable />
+    </Page>
   );
 }
