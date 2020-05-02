a7bbd2f7411292e4643c39c95e5c82984133ad3b
abhinavdc <abhinavdinesh95@gmail.com>
2020-05-02 18:26:19 +0530

    search api

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { filter } from 'lodash';
 import db from './lib/db';
 import './lib/cron';
 
@@ -13,6 +14,18 @@ app.get(`/all-jobs`, async (req, res, next) => {
 
   // respond with json
   res.json(tpJobs);
+});
+
+app.get(`/search-jobs`, async (req, res, next) => {
+  // get the scrape data
+  const { tpJobs } = db.value();
+  const query = req.query.query.toLowerCase();
+
+  const filteredList = filter(
+    tpJobs,
+    (row) => row.jobTitle.toLowerCase().indexOf(query) > -1
+  );
+  return res.json(filteredList);
 });
 
 app.listen(2093, () => {
