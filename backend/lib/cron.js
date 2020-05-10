c9027e7bf7d00337f70fb01d90c4b47135f48473
abhinavdc <abhinavdinesh95@gmail.com>
2020-05-10 23:20:19 +0530

    cron job set to every data 11 PM

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -3,7 +3,7 @@ import { runCron } from './techoparkJobsScraper';
 import { runCron2 } from './infoparkJobsScraper';
 import { runCron3 } from './cyberparkJobsScraper';
 
-cron.schedule('* 23 * * *', () => {
+cron.schedule('0 23 * * *', () => {
   console.log('RUNNING CRON JOB');
   runCron();
   runCron2();
