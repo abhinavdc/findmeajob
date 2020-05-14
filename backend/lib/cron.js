4ca1efe1a4572b40a3b46da85993cfe9eb414587
abhinavdc <abhinavdinesh95@gmail.com>
2020-05-14 11:52:27 +0530

    cron time changed

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -3,17 +3,17 @@ import { runCron } from './techoparkJobsScraper';
 import { runCron2 } from './infoparkJobsScraper';
 import { runCron3 } from './cyberparkJobsScraper';
 
-cron.schedule('25 1 * * *', () => {
+cron.schedule('30 23 * * *', () => {
   console.log('RUNNING CRON JOB 1');
   runCron();
 });
 
-cron.schedule('35 1 * * *', () => {
+cron.schedule('40 23 * * *', () => {
   console.log('RUNNING CRON JOB 2');
   runCron2();
 });
 
-cron.schedule('40 1 * * *', () => {
+cron.schedule('50 23 * * *', () => {
   console.log('RUNNING CRON JOB 3');
   runCron3();
 });
