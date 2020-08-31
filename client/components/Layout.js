7c15daf60b93562636186cd6eb860e662bf0a749
abhinavdc <abhinavdinesh95@gmail.com>
2020-08-31 23:49:50 +0530

    add fav icon

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { hotjar } from 'react-hotjar';
+import Head from 'next/head';
+import favicon from '../assets/favicon.ico';
 
 const HOTJAR_ID = 1968258;
 export default class Layout extends React.Component {
@@ -7,6 +9,13 @@ export default class Layout extends React.Component {
     hotjar.initialize(HOTJAR_ID, 6);
   }
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <div>
+        <Head>
+          <link rel="shortcut icon" href={favicon} />
+        </Head>
+        <div>{this.props.children}</div>
+      </div>
+    );
   }
 }
