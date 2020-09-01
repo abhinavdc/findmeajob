c5a45780bf35c2df7ccab62eeb5c43adce85b5fe
abhinavdc <abhinavdinesh95@gmail.com>
2020-09-01 17:13:42 +0530

    production icons missing fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,2 +1,20 @@
 const withImages = require('next-images');
-module.exports = withImages();
+
+module.exports = withImages({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'static/webfonts/',
+            publicPath: '../webfonts/',
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    });
+    return config;
+  },
+});
